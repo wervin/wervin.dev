71456a178653b17db4d34fcb0720357269e4734e
wervin <fruitaccount@live.com>
2024-01-02 22:32:48 +0100

    Add intial website

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # wervin.dev
 My portfolio website
+
+## TODO
++ experience section
++ competences section
++ projects section
---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('currentYear').innerHTML = new Date().getFullYear();
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
