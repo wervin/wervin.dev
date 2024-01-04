4a778a61964523528211b529b662cbfad926dd42
wervin <fruitaccount@live.com>
2024-01-04 15:19:11 +0100

    Refactor a little bit

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 document.getElementById('currentYear').innerHTML = new Date().getFullYear();
 
 document.addEventListener("DOMContentLoaded", function () {
-  const icon = document.getElementById("mobile-menu");
+  const icon = document.getElementById("menu-icon");
+  const wervin = document.getElementById("wervin-icon");
+  const container = document.querySelector(".header-container");
   const menu = document.querySelector(".menu-links");
 
   icon.addEventListener("click", function () {
-      menu.classList.toggle("open");
-      icon.classList.toggle("open");
+    container.classList.toggle("open");
+    menu.classList.toggle("open");
+  });
+
+  wervin.addEventListener("click", function () {
+    container.classList.remove("open");
+    menu.classList.remove("open");
+    document.documentElement.scrollTop = 0;
+});
+
+  menu.addEventListener("click", function () {
+    container.classList.toggle("open");
+    menu.classList.toggle("open");
   });
 });
